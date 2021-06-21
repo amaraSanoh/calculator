2de145a0aee99b22daa2919dd7ab16f87eabec2d
amarasanoh <sanohawa@gmail.com>
2021-06-21 10:01:26 +0200

    Design Patter : DP METHOD to solve Multiplication

--- src/Controller/Api/ComputeController.php
+++ src/Controller/Api/ComputeController.php
@@ -5,15 +5,16 @@ namespace App\Controller\Api;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\{Request, JsonResponse};
-use App\Solver\{DivisionSolver, SumSolver, SoustractionSolver};
+use App\Solver\{DivisionSolver, MultiplicationSolver, SumSolver, SoustractionSolver};
 
 class ComputeController extends AbstractController
 {
 
     public function __construct(
+        private DivisionSolver $divisionSolver,
+        private MultiplicationSolver $multiplicationSolver,
         private SumSolver $sumSolver,
-        private SoustractionSolver $soustractionSolver,
-        private DivisionSolver $divisionSolver
+        private SoustractionSolver $soustractionSolver
     ){}
 
     #[Route('/api/v1/compute', name: 'api_compute')]
@@ -21,6 +22,7 @@ class ComputeController extends AbstractController
     {
         if(null !== $expression = json_decode($request->getContent())?->expression) {
             $compute = $this->divisionSolver->solve($expression);
+            $compute = $this->multiplicationSolver->solve($compute);
             $compute = $this->sumSolver->solve($compute);
             $compute = $this->soustractionSolver->solve($compute);
             return $this->json([
