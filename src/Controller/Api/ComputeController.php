ba5bd66e1d449cf827f7c7f2ebffd15e3f5bd77a
amarasanoh <sanohawa@gmail.com>
2021-06-21 09:52:55 +0200

    Design Patter : DP METHOD to solve Diviion

--- src/Controller/Api/ComputeController.php
+++ src/Controller/Api/ComputeController.php
@@ -5,21 +5,23 @@ namespace App\Controller\Api;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\{Request, JsonResponse};
-use App\Solver\{SumSolver, SoustractionSolver};
+use App\Solver\{DivisionSolver, SumSolver, SoustractionSolver};
 
 class ComputeController extends AbstractController
 {
 
     public function __construct(
         private SumSolver $sumSolver,
-        private SoustractionSolver $soustractionSolver
+        private SoustractionSolver $soustractionSolver,
+        private DivisionSolver $divisionSolver
     ){}
 
     #[Route('/api/v1/compute', name: 'api_compute')]
     public function compute(Request $request): JsonResponse
     {
         if(null !== $expression = json_decode($request->getContent())?->expression) {
-            $compute = $this->sumSolver->solve($expression);
+            $compute = $this->divisionSolver->solve($expression);
+            $compute = $this->sumSolver->solve($compute);
             $compute = $this->soustractionSolver->solve($compute);
             return $this->json([
                 'compute' => $compute
---
+++ src/Solver/DivisionSolver.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Solver;
+use App\Solver\AbstractSolver;
+
+class DivisionSolver extends AbstractSolver
+{
+    protected function getOperator(): string
+    {
+       return '/';
+    }
+
+    protected function compute(string $operand1, string $operand2): string
+    {
+        return $operand1 / $operand2;
+    }
+
+    protected function getRegex(): string
+    {
+        return "#(\d+\/\d+)#";
+    }
+}
