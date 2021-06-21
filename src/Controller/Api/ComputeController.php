00b7e96bc619e1d80312b3caa3186a70707e1c69
amarasanoh <sanohawa@gmail.com>
2021-06-21 09:48:44 +0200

    Design Patter : DP METHOD to solve Soustraction

--- src/Controller/Api/ComputeController.php
+++ src/Controller/Api/ComputeController.php
@@ -5,13 +5,14 @@ namespace App\Controller\Api;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\{Request, JsonResponse};
-use App\Solver\{SumSolver};
+use App\Solver\{SumSolver, SoustractionSolver};
 
 class ComputeController extends AbstractController
 {
 
     public function __construct(
-        private SumSolver $sumSolver
+        private SumSolver $sumSolver,
+        private SoustractionSolver $soustractionSolver
     ){}
 
     #[Route('/api/v1/compute', name: 'api_compute')]
@@ -19,6 +20,7 @@ class ComputeController extends AbstractController
     {
         if(null !== $expression = json_decode($request->getContent())?->expression) {
             $compute = $this->sumSolver->solve($expression);
+            $compute = $this->soustractionSolver->solve($compute);
             return $this->json([
                 'compute' => $compute
             ]);
